4f0c053b67589e977e0700a69a8f738b8b28b05f
Badji87 <mradovanovic@msn.com>
2023-01-18 21:03:47 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing one',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cupiditate delectus deleniti dignissimos doloremque hic ipsum magnam, molestiae natus nostrum obcaecati odio possimus qui quibusdam ratione temporibus totam, voluptate!'
+
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cupiditate delectus deleniti dignissimos doloremque hic ipsum magnam, molestiae natus nostrum obcaecati odio possimus qui quibusdam ratione temporibus totam, voluptate!'
+            ]
+        ]
+    ]);
 });
 
-Route::get('/hello', function () {
-    return response('<h1>Hello World!</h1>', 200)->header('Content-Type', 'text/plain')->header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function ($id) {
-    ddd($id);
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-
-Route::get('/search',function (Request $request){
-return $request->ime . ' ' . $request->grad;
-});
+//Route::get('/hello', function () {
+//    return response('<h1>Hello World!</h1>', 200)->header('Content-Type', 'text/plain')->header('foo', 'bar');
+//});
+//
+//Route::get('/posts/{id}', function ($id) {
+//    ddd($id);
+//    return response('Post ' . $id);
+//})->where('id', '[0-9]+');
+//
+//Route::get('/search',function (Request $request){
+//return $request->ime . ' ' . $request->grad;
+//});

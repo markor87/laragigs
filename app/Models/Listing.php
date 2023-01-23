cc24ad7a12751f195a735800ec2f4bc89df2ea53
Badji87 <mradovanovic@msn.com>
2023-01-23 20:50:55 +0100

    commit

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing one',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cupiditate delectus deleniti dignissimos doloremque hic ipsum magnam, molestiae natus nostrum obcaecati odio possimus qui quibusdam ratione temporibus totam, voluptate!'
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing two',
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cupiditate delectus deleniti dignissimos doloremque hic ipsum magnam, molestiae natus nostrum obcaecati odio possimus qui quibusdam ratione temporibus totam, voluptate!'
-            ]
-        ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id)
-                return $listing;
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
